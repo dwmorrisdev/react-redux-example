997ddd58bd33ee962a42acd5e59380f88b0d4826
Dustin Morris <dwmorris.biz@outlook.com>
2017-09-21 10:38:37 -0500

    add basic redux

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,4 +1,31 @@
 var redux = require('redux');
 
 console.log('starting redux example');
- 
+
+var stateDefault = {
+  name: 'Anonymous'
+};
+
+var reducer = (state = stateDefault, action) => {
+  //state = state || {name: 'Anonymous'};
+  console.log('New action: ', action);
+  switch (action.type){
+      case 'CHANGE_NAME':
+        return {
+          ...state,
+          name: action.name
+        };
+      default:
+        return state;
+  }
+};
+var store = redux.createStore(reducer);
+
+console.log('currentState: ', store.getState());
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Dustin'
+});
+
+console.log('name should be Dustin', store.getState());
